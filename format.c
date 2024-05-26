8e0ccc6d76af26c83e5adebfaa18aef364e59975
default <nobody@localhost>
2024-05-26 09:27:36 +0200

    Added markdown-like links.

--- format.c
+++ format.c
@@ -87,7 +87,12 @@ static xs_str *format_line(const char *line, xs_list **attach)
 
     /* split by markup */
     xs *sm = xs_regex_split(line,
-        "(`[^`]+`|\\*\\*?[^\\*]+\\*?\\*|https?:/" "/[^[:space:]]+)");
+        "("
+            "`[^`]+`"                   "|"
+            "\\*\\*?[^\\*]+\\*?\\*"     "|"
+            "\\[[^]]+\\]\\([^\\)]+\\)"  "|"
+            "https?:/" "/[^[:space:]]+"
+        ")");
     int n = 0;
 
     p = sm;
@@ -134,6 +139,16 @@ static xs_str *format_line(const char *line, xs_list **attach)
                     xs *s1 = xs_fmt("<a href=\"%s\" target=\"_blank\">%s</a>", v2, u);
                     s = xs_str_cat(s, s1);
                 }
+            }
+            else
+            if (*v == '[') {
+                /* markdown-like links [label](url) */
+                xs *w    = xs_strip_chars_i(xs_dup(v), "[)");
+                xs *l    = xs_split(w, "](");
+                xs *link = xs_fmt("<a href=\"%s\">%s</a>",
+                            xs_list_get(l, 1), xs_list_get(l, 0));
+
+                s = xs_str_cat(s, link);
             }
             else
                 s = xs_str_cat(s, v);
