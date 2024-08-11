501912d28cb8044f891c990cdf298ff449a033cd
default <nobody@localhost>
2024-08-11 17:29:51 +0200

    Bumped version.

--- snac.h
+++ snac.h
@@ -1,7 +1,7 @@
 /* snac - A simple, minimalistic ActivityPub instance */
 /* copyright (c) 2022 - 2024 grunfink et al. / MIT license */
 
-#define VERSION "2.57"
+#define VERSION "2.58-dev"
 
 #define USER_AGENT "snac/" VERSION
 
